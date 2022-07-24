703dd6cd455c91a1acaf65b5536021bde18a3a42
andikscript <andikscript@gmail.com>
2022-07-24 20:47:38 +0700

    remove response message unused

--- src/main/java/com/andikscript/databasemigration/repository/SaveRepository.java
+++ src/main/java/com/andikscript/databasemigration/repository/SaveRepository.java
@@ -1,6 +1,5 @@
 package com.andikscript.databasemigration.repository;
 
-import com.andikscript.databasemigration.message.SaveMessage;
 import com.andikscript.databasemigration.model.Save;
 import org.springframework.data.jpa.repository.JpaRepository;
 
