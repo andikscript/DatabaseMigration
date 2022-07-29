1c6bcd4e432fa2e27935e133c29530d91969da16
andikscript <andikscript@gmail.com>
2022-07-29 16:33:30 +0700

    add annotation for repository

--- src/main/java/com/andikscript/databasemigration/repository/SaveRepository.java
+++ src/main/java/com/andikscript/databasemigration/repository/SaveRepository.java
@@ -2,6 +2,8 @@ package com.andikscript.databasemigration.repository;
 
 import com.andikscript.databasemigration.model.Save;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SaveRepository extends JpaRepository<Save, Integer> {
 }
