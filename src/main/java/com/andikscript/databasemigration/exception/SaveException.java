1c7bdca821c6d84bf1694e2fc2a7707fe27cae4b
andikscript <andikscript@gmail.com>
2022-08-12 04:42:33 +0700

    add exception override from class parent for handle if not valid value on save

--- src/main/java/com/andikscript/databasemigration/exception/SaveException.java
+++ src/main/java/com/andikscript/databasemigration/exception/SaveException.java
@@ -1,13 +1,24 @@
 package com.andikscript.databasemigration.exception;
 
+import com.andikscript.databasemigration.error.ApiError;
 import org.hibernate.PropertyValueException;
+import org.hibernate.TypeMismatchException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
 public class SaveException  extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(PropertyValueException.class)
@@ -15,4 +26,16 @@ public class SaveException  extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
                 .body(ex.getMessage());
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ApiError(
+                        HttpStatus.BAD_REQUEST,
+                        LocalDateTime.now(),
+                        "Validate Error",
+                        ex.getBindingResult().toString()
+                ));
+}
 }
